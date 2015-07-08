15ea1fdfa73e0926b861b9fe75adaf6d18c31a91
electrikjesus <electrikjesus@gmail.com>
2015-07-08 09:22:51 -0400

    removed Bliss tags

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -84,18 +84,3 @@ BOARD_HAS_NO_REAL_SDCARD := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := /sys/class/backlight/pwm-backlight/brightness
 TW_MAX_BRIGHTNESS := 255
-
-# BlissPop Configs
-TARGET_TC_ROM := 4.8-linaro
-TARGET_TC_KERNEL := 4.8-linaro
-BLISSIFY := true
-BLISS_O3 := true
-BLISS_STRICT := false
-BLISS_GRAPHITE := true
-BLISS_KRAIT := false
-BLISS_PIPE := true
-TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
-
-#SaberMod
--include vendor/bliss/config/sm.mk
